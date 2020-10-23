862cb99659b5945d8100a9508346d04809b83cbd
Szymon Kaczorowski <szymonk@kainos.com>
2020-10-23 11:41:14 +0200

    feature/dist-zip: fix

--- webpack.production.js
+++ webpack.production.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs-extra')
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const archiver = require('archiver');
@@ -6,39 +6,43 @@ const branchName = require('current-git-branch');
 
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const MinifyBundledPlugin = require('minify-bundled-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 const LAMBDA_NAME = 'ATFAVAILABILITY';
 const OUTPUT_FOLDER = './dist'
 const BUILD_VERSION = branchName().replace("/","-");
 
-class ZipPlugin {
+class BundlePlugin {
   constructor(params) {
-    this.inputPath = params.inputPath;
-    this.outputPath = params.outputPath;
-    this.outputName = params.outputName;
+    this.archives = params.archives
   }
 
   apply(compiler) {
     compiler.hooks.afterEmit.tap('zip-pack-plugin', async (compilation) => {
-      if (!fs.existsSync(this.outputPath)) {
-        fs.mkdirSync(this.outputPath)
-      };
-      const output = fs.createWriteStream(`${this.outputPath}/${this.outputName}.zip`);
-      const archive = archiver('zip');
-
-      output.on('close', function () {
-        console.log(archive.pointer() + ' total bytes');
-        console.log('archiver has been finalized and the output file descriptor has closed.');
-      });
-      archive.on('error', function(err){
-          throw err;
-      });
-      
-      archive.pipe(output);
-      archive.directory(`${this.inputPath}`, false);
-      await archive.finalize();
+      this.archives.forEach(async (archive) => {
+        await this.createArchive(archive.inputPath, archive.outputPath, archive.outputName);
+      })
+      fs.copySync('./.aws-sam', './dist/.aws-sam');
     });
+  }
+
+  createArchive(inputPath, outputPath, outputName) {
+    if (!fs.existsSync(outputPath)) {
+      fs.mkdirSync(outputPath)
+    };
+    const output = fs.createWriteStream(`${outputPath}/${outputName}.zip`);
+    const archive = archiver('zip');
+
+    output.on('close', function () {
+      console.log(archive.pointer() + ' total bytes');
+      console.log('archiver has been finalized and the output file descriptor has closed.');
+    });
+    archive.on('error', function(err){
+        throw err;
+    });
+    
+    archive.pipe(output);
+    archive.directory(`${inputPath}`, false);
+    return archive.finalize();
   }
 };
 
@@ -48,10 +52,14 @@ module.exports = merge(common, {
     new MinifyBundledPlugin({
       patterns: [`.aws-sam/**/*.js`],
     }),
-    new ZipPlugin({
-      inputPath: `./.aws-sam/build/${LAMBDA_NAME}`,
-      outputPath: `${OUTPUT_FOLDER}`,
-      outputName: `HVT-${LAMBDA_NAME}-${BUILD_VERSION}`
+    new BundlePlugin({
+      archives: [
+        {
+          inputPath: `./.aws-sam/build/${LAMBDA_NAME}`,
+          outputPath: `${OUTPUT_FOLDER}`,
+          outputName: `HVT-${LAMBDA_NAME}-${BUILD_VERSION}`
+        }
+      ]
     }),
   ],
   optimization: {
